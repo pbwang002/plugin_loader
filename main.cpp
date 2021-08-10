1bbe7410b30729be2183acc306cb4b7ff3b42fab
王攀博 <wangpanbo@huawei.com>
2021-08-10 16:26:18 +0800

    updata doc

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
-#include <iostream>
-#include <string>
-#include <thread>
-#include "plugin_manager/plugin_manager.h"
-#include "plugins/add/add.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef int (*ADD)(int x, int y);
-int main()
+int main(int argc, char* argv[])
 {
-    std::string basePluginPath = "/mnt/d/code/code_hub/plugin_loader/build/lib/";
-    std::string addPlugin = "libadd.so";
-    PluginManager pluginManager;
-    // auto plugin = pluginManager.LoadPlugin(basePluginPath + addPlugin);
-    // auto add_func = (ADD)plugin->GetFunc("Add");
-    // auto ret = add_func(1, 2);
-    // std::cout << ret << std::endl;
-    auto ret = Add(1, 2);
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
-    }
-    
+    printf("hello: ");
+    printf("world");
+    printf("Down\n");
+
     return 0;
 }
